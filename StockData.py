7f5eee9c49f4454619b410527f3cd9fa8507d989
lei <wjlei1990@gmail.com>
2014-12-27 22:53:09 -0500

    A few Updates:
    1) build database connection with sqlalchemy service
    2) add database update function

--- StockData.py
+++ StockData.py
@@ -40,7 +40,7 @@ class StockOnline(object):
         if start > end:
             raise ValueError("StartTime is larger than EndTime")
 
-        print "\n+++++++++++++++++"
+        #print "\n+++++++++++++++++\nPull Data Service"
         stock_table = {}
         for stock in stock_list:
             print "Pull Stock : %s ..." % stock
@@ -51,6 +51,9 @@ class StockOnline(object):
                 raise RuntimeError("Can't not download Stock: %s" % stock)
 
         return stock_table
+
+class StockSqlObj(object):
+    pass
 
 class StockServer(object):
 
@@ -77,7 +80,7 @@ class StockServer(object):
         :param init_mode: to_sql if_exist variable
         :return:
         """
-        print "\n+++++++++++++++++"
+        print "\n++++++++++++++++\nInit DB Service"
         for stock_name, stock_data in stock_table.iteritems():
             print "Init Table Name in db: %s" %stock_name
             if stock_data is None:
@@ -95,24 +98,74 @@ class StockServer(object):
                     except:
                         raise RuntimeError("Can not init table in database for stock %s" %stock_name)
 
-    def update_db(self, stock_list, engine):
+    # ----------------------------------------------------------------------
+    def loadsession(self, stock_name):
+        """"""
+        from sqlalchemy import MetaData, Table
+        from sqlalchemy.orm import mapper, sessionmaker
+        from sqlalchemy import Column, DATETIME
+
+        metadata = MetaData(self.engine)
+        stocktable = Table(stock_name, metadata, Column("Date", DATETIME, primary_key=True),autoload=True)
+        #mapper(StockSqlObj, stocktable)
+
+        T1Foo = self.map_class_to_some_table(StockSqlObj, stocktable, stock_name)
+
+        session = self.create_session()
+        return session, T1Foo
+
+    @staticmethod
+    def map_class_to_some_table(cls, table, entity_name, **kw):
+        from sqlalchemy.orm import mapper
+        newcls = type(entity_name, (cls, ), {})
+        mapper(newcls, table, **kw)
+        return newcls
+
+    def create_session(self):
+        from sqlalchemy.orm import mapper, sessionmaker
+        session = sessionmaker(bind=self.engine)
+        session = session()
+        return session
+
+    def get_largest_date(self, stock_name):
+        from sqlalchemy.sql import func
+        session, stockobj = self.loadsession(stock_name)
+        qry = session.query(func.max(stockobj.Date).label("max_date"))
+        res = qry.one()
+        return res.max_date
+
+    def update_db(self, stock_list):
         """
         Function that updates the database to current date
         :param engine:
         :return:
         """
-        for stock in stock_list:
-            # get end time
-            now = datetime.datetime.now()
-            end = datetime.datetime(temp_now.year, temp_now.month, temp_now.day)
-            # get start time
-            # start =
+        print "\n++++++++++++++++++\nUpdate Table Service"
 
-            table_exist=pandas.io.sql.has_table(stock, engine)
-            #if table_exist:
-                # update
-            #else:
-                # just return
+        onlinedata = StockOnline()
+
+        for stock_name in stock_list:
+            print "Update table: %s" %stock_name
+            table_exist=pandas.io.sql.has_table(stock_name, self.engine)
+            print "Table exist: %s" % table_exist
+            if table_exist:
+                print "Update now..."
+                # get end time
+                now = datetime.datetime.now()
+                endtime = datetime.datetime(now.year, now.month, now.day)
+                print type(endtime), endtime
+                # get start time
+                starttime = self.get_largest_date(stock_name) + datetime.timedelta(days=1)
+                print type(starttime), starttime
+                stock_table = onlinedata.pull_data((stock_name,), starttime, endtime)
+                #write into db
+                if stock_table[stock_name] is not None:
+                    print "Online Data is not None. Updating..."
+                    stock_table[stock_name].to_sql(stock_name, self.engine, if_exists='append')
+                else:
+                    print "Online Data is None. Skip it..."
+            else:
+                "Skip this stock"
 
 
 class StockClient(object):
@@ -174,7 +227,6 @@ class StockClient(object):
             else:
                 boolen_dict[stock] = False
         return boolen_dict
-
 
 
 
